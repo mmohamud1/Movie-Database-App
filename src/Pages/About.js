02976489a58183d46af2e17abcd6f05748f59da2
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-08-04 03:26:03 +0200

    nav style

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,9 +4,9 @@ const About = () => {
   return (
     <div className="all-center py-3 my-3">
       <h1 className="my-1 uppercase underline">About This App</h1>
-      <p>App to search movies from 'The OMDB API'</p>
+      <p>App to search for movies from 'The OMDB API'</p>
       <p>Version: 1.0.0</p>
-      <p>Created by Mohamed Mohamud</p>
+      <p>Created by Mohamed Mohamud &copy;</p>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
     return (
         <nav className="navbar bg-primary">
             <img className="navbar-img" src={Logo} alt="logo"/>  ≈
-            <h1>
-                Movie Database
+            <h1 className="navbar-title">
+            Movie Database
             </h1>
             <ul>
                 <li>
